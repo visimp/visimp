ef1494267b43fac8503e8f44887cf8cb4bb06b5c
Luca <luca.tagliavini5@studio.unibo.it>
2021-10-12 14:47:19 +0200

    add vimtex

--- lua/visimp/languages/latex.lua
+++ lua/visimp/languages/latex.lua
@@ -2,7 +2,10 @@ local L = require('visimp.layer').new_layer('latex')
 local layers = require('visimp.loader')
 
 -- TODO: add latex language server
-L.default_config = {}
+L.default_config = {
+  -- Install and enable the vimtex plugin
+  vimtex = true
+}
 
 function L.dependencies()
   return {'treesitter'}
@@ -12,4 +15,9 @@ function L.preload()
   -- Configure treesitter
   local ts = layers.get('treesitter')
   ts.langs({'latex'})
+
+  if L.config.vimtex then
+    package('lervag/vimtex')
+    vim.cmd('packadd vimtex')
+  end
 end
