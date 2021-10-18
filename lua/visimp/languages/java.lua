98774426db29acefa6a91421c8aabc739b4d5d6f
Luca <luca.tagliavini5@studio.unibo.it>
2021-10-18 19:34:38 +0200

    add java support
    Closes #11

---
+++ lua/visimp/languages/java.lua
@@ -0,0 +1,31 @@
+local L = require('visimp.layer').new_layer('java')
+local layers = require('visimp.loader')
+
+L.default_config = {
+  -- The lsp server to use. Defaults to eclipse's jdtls downloaded via standard
+  -- POSIX tools, but users can also provide their own installation.
+  lsp = nil,
+  -- Optional configuration to be provided for the chosen language server
+  lspconfig = nil
+}
+
+function L.dependencies()
+  local deps = {'treesitter'}
+  if L.config.lsp ~= false then
+    table.insert(deps, 'lsp')
+  end
+  return deps
+end
+
+function L.preload()
+  -- Configure treesitter
+  layers.get('treesitter').langs({'java'})
+
+  -- Enable the language server
+  if L.config.lsp ~= false then
+    local lsp = layers.get('lsp')
+    lsp.use_server('java', L.config.lsp, L.config.lspconfig)
+  end
+end
+
+return L
