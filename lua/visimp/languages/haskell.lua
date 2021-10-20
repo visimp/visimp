977c43a96676df85b979464f36253af1fc78b1f6
Luca <luca.tagliavini5@studio.unibo.it>
2021-10-20 13:29:25 +0200

    haskell: add support

---
+++ lua/visimp/languages/haskell.lua
@@ -0,0 +1,31 @@
+local L = require('visimp.layer').new_layer('haskell')
+local layers = require('visimp.loader')
+
+L.default_config = {
+  -- The lsp server to use. Defaults to nil which installs hls or can be
+  -- disabled by setting it to false
+  lsp = nil,
+  -- Optional configuration to be provided for the chosen language server
+  lspconfig = nil
+}
+
+function L.dependencies()
+  local deps = {'treesitter'}
+  if L.config.lsp ~= false then
+    table.insert(deps, 'lsp')
+  end
+  return deps
+end
+
+function L.preload()
+  -- Configure treesitter
+  layers.get('treesitter').langs({'haskell'})
+
+  -- Enable the language server
+  if L.config.lsp ~= false then
+    layers.get('lsp').use_server('haskell',
+      L.config.lsp == nil, L.config.lsp or 'hls', L.config.lspconfig)
+  end
+end
+
+return L
