1ee4fa512e97135328a5bf2fe56474fd077095e1
Luca <luca.tagliavini5@studio.unibo.it>
2021-10-20 17:59:53 +0200

    ampl: add support

---
+++ lua/visimp/languages/ampl.lua
@@ -0,0 +1,8 @@
+local L = require('visimp.layer').new_layer('ampl')
+
+function L.preload()
+  -- Let vim recognize .mod files as AMPL
+  vim.cmd('au! BufRead,BufNewFile *.mod setfiletype ampl')
+end
+
+return L
