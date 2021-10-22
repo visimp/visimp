dc0dde6a134432466e32a69582826ee6852eb0bd
Luca <luca.tagliavini5@studio.unibo.it>
2021-10-22 09:33:57 +0200

    simplify comment layer

--- lua/visimp/layers/comment.lua
+++ lua/visimp/layers/comment.lua
@@ -1,23 +1,14 @@
 local L = require('visimp.layer').new_layer('comment')
 local get_module = require('visimp.utils').get_module
 
-L.default_config = {
-  -- Set to true to enable commands provided by the nvim-comment plugin
-  commands = false,
-  config = {}
-}
+L.default_config = {}
 
 function L.packages()
   return {'terrortylor/nvim-comment'}
 end
 
 function L.load()
-  if L.config.commands then
-    vim.cmd('packadd nvim-comment')
-  end
-
-  local comment = get_module('nvim_comment')
-  comment.setup(L.config.config or {})
+  get_module('nvim_comment').setup(L.config or {})
 end
 
 return L
