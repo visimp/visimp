2a7ccfbae7df1cffa2eec96f955b245d437b270d
Luca <luca.tagliavini5@studio.unibo.it>
2021-10-27 11:04:20 +0200

    lua: add language

---
+++ lua/visimp/languages/lua.lua
@@ -0,0 +1,35 @@
+local L = require('visimp.layer').new_layer('lua')
+local layers = require('visimp.loader')
+
+L.default_config = {
+  -- Leave to nil to use sumneko_lua LSP, otherwhise can specify local
+  -- installation or disable by setting to false.
+  lsp = nil,
+  -- Optional configuration to be provided for the chosen language server
+  lspconfig = nil,
+}
+
+function L.dependencies()
+  local deps = { 'treesitter' }
+  if L.config.lsp ~= false then
+    table.insert(deps, 'lsp')
+  end
+  return deps
+end
+
+function L.preload()
+  -- Configure treesitter
+  layers.get('treesitter').langs({ 'lua' })
+
+  -- Enable the language server
+  if L.config.lsp ~= false then
+    layers.get('lsp').use_server(
+      'sumneko_lua',
+      L.config.lsp == nil,
+      L.config.lsp or 'sumneko_lua',
+      L.config.lspconfig
+    )
+  end
+end
+
+return L
