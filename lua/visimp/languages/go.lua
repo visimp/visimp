c53101a0556b83fe33464faeb7e83768bf029b87
Luca <luca.tagliavini5@studio.unibo.it>
2021-10-27 18:43:03 +0200

    go: added

---
+++ lua/visimp/languages/go.lua
@@ -0,0 +1,35 @@
+local L = require('visimp.layer').new_layer('go')
+local layers = require('visimp.loader')
+
+L.default_config = {
+  -- Leave to nil to use the phpactor LSP, false to disable, a string to use a
+  -- local binary
+  lsp = nil,
+  -- Optional configuration to be provided for the chosen language server
+  lspconfig = nil
+}
+
+function L.dependencies()
+  local deps = { 'treesitter' }
+  if L.config.lsp ~= false then
+    table.insert(deps, 'lsp')
+  end
+  return deps
+end
+
+function L.preload()
+  -- Configure treesitter
+  layers.get('treesitter').langs({ 'go' })
+
+  -- Enable the language server
+  if L.config.lsp ~= false then
+    layers.get('lsp').use_server(
+      'go',
+      L.config.lsp == nil,
+      L.config.lsp or 'gopls',
+      L.config.lspconfig
+    )
+  end
+end
+
+return L
