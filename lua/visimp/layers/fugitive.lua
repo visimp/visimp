320643e89979f21dfce7ea5b1a3fa74c80904f2e
Luca <luca.tagliavini5@studio.unibo.it>
2021-10-28 14:30:20 +0200

    fugitive: add layer
    Closes #30

---
+++ lua/visimp/layers/fugitive.lua
@@ -0,0 +1,13 @@
+local L = require('visimp.layer').new_layer('fugitive')
+
+L.default_config = {}
+
+function L.packages()
+  return { 'tpope/vim-fugitive' }
+end
+
+function L.preload()
+  vim.cmd('packadd vim-fugitive')
+end
+
+return L
