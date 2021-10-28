59011a0b3f02934da8e098bf4e3b3e51fb6d57cd
Luca <luca.tagliavini5@studio.unibo.it>
2021-10-28 09:09:54 +0200

    update default configuration

--- _init.lua
+++ _init.lua
@@ -1,15 +1,24 @@
 require'visimp'{
-  {
-    defaults = {
-      foldmethod = 'marker'
-    },
-    languages = {
-      'c', 'python', 'latex'
-    },
-    python = {
-      lsp = 'pyright' -- Avoid installing pyright, use the system's default
-    },
-    -- theme = gruvbox()
-    theme = {'lifepillar/vim-gruvbox8', 'gruvbox8', 'dark'}
-  }
+  languages = {
+    'c',
+    -- 'ampl',
+    -- 'bash',
+    -- 'c',
+    -- 'csharp',
+    -- 'css',
+    -- 'dart',
+    -- 'go',
+    -- 'haskell',
+    -- 'html',
+    -- 'java',
+    -- 'javascript',
+    -- 'json',
+    -- 'latex',
+    -- 'lua',
+    -- 'php',
+    -- 'python',
+    -- 'toml'
+  },
+  lspsignature = {},
+  theme = {'lifepillar/vim-gruvbox8', 'gruvbox8', 'dark'}
 }
