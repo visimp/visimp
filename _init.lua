badfdf8dea9bf84781b5e7bbc50c125e581c546f
Luca <luca.tagliavini5@studio.unibo.it>
2021-11-03 18:02:09 +0100

    improve docs in the default init file

--- _init.lua
+++ _init.lua
@@ -19,6 +19,17 @@ require'visimp'{
     -- 'python',
     -- 'toml'
   },
-  lspsignature = {},
+
+  -- Any extra layers can be enabled or configured (which implicitly enabled
+  -- them) by adding a field in this configuration. Let's say we want to eanble
+  -- the `lspsignature` layer, this is what we'd add:
+  -- lspsignature = {},
+  --
+  -- Layers can also be disabled by setting a key in this configuration to false
+  -- For example the following line disabled the `lspsignature` layer:
+  -- lspsignature = false
+
+  --       package url                theme name  background
+  --       github username/repo       a string    either dark or bright
   theme = {'lifepillar/vim-gruvbox8', 'gruvbox8', 'dark'}
 }
