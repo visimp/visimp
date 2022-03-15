22d3449ade6151348ae3fac8ec889fad394d2e93
Luca <luca.tagliavini5@studio.unibo.it>
2022-03-15 16:57:16 +0100

    layer(lspformat): hook into the lsp layer at the right time

--- lua/visimp/layers/lspformat.lua
+++ lua/visimp/layers/lspformat.lua
@@ -6,7 +6,7 @@ L.default_config = {
   -- Aplies an autocommand to fix the behavious when quitting and wiring.
   -- Because the formatter is async by deafult the code wouldn't be patched
   -- without this fix when wiring and closing the editor at the same time.
-  wq_fix = true
+  wq_fix = true,
 }
 
 function L.dependecies()
@@ -17,15 +17,19 @@ function L.packages()
   return { 'lukas-reineke/lsp-format.nvim' }
 end
 
+function L.preload()
+  layers.get('lsp').on_attach(get_module('lsp-format').on_attach)
+end
+
 function L.load()
   if L.config.wq_fix then
     -- Taken from https://github.com/lukas-reineke/lsp-format.nvim#wq-will-not-format
-    vim.cmd [[cabbrev wq execute 'lua vim.lsp.buf.formatting_seq_sync()' <bar> wq]]
+    vim.cmd(
+      [[cabbrev wq execute 'lua vim.lsp.buf.formatting_seq_sync()' <bar> wq]]
+    )
   end
 
-  local lspformat = get_module('lsp-format')
-  layers.get('lsp').on_attach(lspformat.on_attach)
-  lspformat.setup(L.config or {})
+  get_module('lsp-format').setup(L.config or {})
 end
 
 return L
