669a59fa56d3118a109ce799534d51221e7a9dc9
Luca <luca.tagliavini5@studio.unibo.it>
2022-07-27 13:46:04 +0200

    added blankline layer

---
+++ lua/visimp/layers/blankline.lua
@@ -0,0 +1,17 @@
+local L = require('visimp.layer').new_layer('blankline')
+local get_module = require('visimp.bridge').get_module
+
+-- See https://github.com/lukas-reineke/indent-blankline.nvim#setup for more
+L.default_config = {}
+
+function L.packages()
+  return {
+    'lukas-reineke/indent-blankline.nvim',
+  }
+end
+
+function L.load()
+  get_module('indent_blankline').setup(L.config or {})
+end
+
+return L
