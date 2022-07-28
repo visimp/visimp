0dd3fb81c416892ed6f14dba0c7b56b8755302a5
Luca <luca.tagliavini5@studio.unibo.it>
2022-07-28 18:06:48 +0200

    simplify binding

--- lua/visimp/layers/binds.lua
+++ lua/visimp/layers/binds.lua
@@ -1,20 +1,9 @@
 local L = require('visimp.layer').new_layer('binds')
-local bind = require('visimp.bind').bind
 
-L.default_config = {
-}
+L.default_config = {}
 
 function L.load()
-  bind(L.config, function(action)
-    if type(action) == 'string' then
-      return function() vim.api.nvim_command(action) end
-    elseif type(action) == 'function' then
-      return action
-    else
-      error('Invalid bind handler type:', type(action))
-      return nil
-    end
-  end)
+  require('visimp.bind').bind(L.config, function(v) return v end)
 end
 
 return L
