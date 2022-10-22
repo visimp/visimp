e75fc5420cff30c6c67fc742060c42237307a00b
Luca <luca.tagliavini5@studio.unibo.it>
2022-10-22 12:27:41 +0200

    add colorizer layer

---
+++ lua/visimp/layers/colorizer.lua
@@ -0,0 +1,15 @@
+local L = require('visimp.layer').new_layer('colorizer')
+local get_module = require('visimp.bridge').get_module
+
+-- All fields from https://github.com/NvChad/nvim-colorizer.lua#customization
+L.default_config = {}
+
+function L.packages()
+  return { 'NvChad/nvim-colorizer.lua' }
+end
+
+function L.load()
+  get_module('colorizer').setup(L.config or {})
+end
+
+return L
