6e78970802086045269191933ea901dd6c3c08a3
Luca <luca.tagliavini5@studio.unibo.it>
2023-03-06 11:17:08 +0100

    address nitpick

--- lua/visimp/layers/icons.lua
+++ lua/visimp/layers/icons.lua
@@ -1,16 +1,14 @@
 local L = require('visimp.layer').new_layer('icons')
 local get_module = require('visimp.bridge').get_module
 
--- for nvim-tree default config see https://github.com/nvim-tree/nvim-tree.lua
-L.default_config = {
-}
+-- for nvim-web-devicons default config see https://github.com/nvim-tree/nvim-web-devicons#setup
+L.default_config = {}
 
 function L.packages()
   return {
-    'nvim-tree/nvim-web-devicons'
+    'nvim-tree/nvim-web-devicons',
   }
 end
-
 
 function L.load()
   get_module('nvim-web-devicons').setup(L.config)
