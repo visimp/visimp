800ab98d0c9fdc44c137b14549c75edde18dd69a
Luca <luca.tagliavini5@studio.unibo.it>
2023-04-18 20:06:40 +0200

    add zen mode layer

---
+++ lua/visimp/layers/zen.lua
@@ -0,0 +1,37 @@
+local L = require('visimp.layer').new_layer('zen')
+local get_module = require('visimp.bridge').get_module
+local get_layer = require('visimp.loader').get
+local opt = require('visimp.bridge').opt
+
+L.default_config = {
+  window = {
+    width = 100,
+  },
+  on_open = function()
+    local colorcolumn = get_layer('defaults').config.colorcolumn
+    if colorcolumn ~= nil then
+      opt('b', 'textwidth', colorcolumn)
+      vim.bo.formatoptions = vim.bo.formatoptions .. 'a'
+    end
+  end,
+  on_close = function()
+    opt('b', 'textwidth', 0)
+    vim.bo.formatoptions = vim.bo.formatoptions:gsub('a', '')
+  end,
+}
+
+function L.dependencies()
+  return { 'defaults' }
+end
+
+function L.packages()
+  return {
+    'folke/zen-mode.nvim',
+  }
+end
+
+function L.load()
+  get_module('zen-mode').setup(L.config)
+end
+
+return L
