838ecfc7e6419240688b850c8eb9cc4215664844
Luca <luca.tagliavini5@studio.unibo.it>
2023-07-20 15:38:36 +0200

    deprecate the grammarly layer; add the ltex alternative

---
+++ lua/visimp/layers/ltex.lua
@@ -0,0 +1,19 @@
+local L = require('visimp.layer').new_layer('ltex')
+local layers = require('visimp.loader')
+
+-- Optional configuration to be provided for the language server
+-- All fields from https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ltex
+-- are accepted here.
+L.default_config = {
+  language = 'en-US',
+}
+
+function L.dependencies()
+  return { 'lsp' }
+end
+
+function L.preload()
+  layers.get('lsp').use_server('ltex', true, 'ltex', L.config or {})
+end
+
+return L
