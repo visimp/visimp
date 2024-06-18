58334bd62def4788011a80c85d9092c1aacc89ec
Stefano Volpe <foxy@teapot.ovh>
2024-06-18 11:46:53 +0200

    feat: ultimate blankline-rainbow integration

--- lua/visimp/layers/blankline.lua
+++ lua/visimp/layers/blankline.lua
@@ -1,6 +1,6 @@
 local L = require('visimp.layer').new_layer 'blankline'
 local get_module = require('visimp.bridge').get_module
-local layers = require('visimp.loader').layers
+local loader = require 'visimp.loader'
 
 L.default_config = {
   -- Any config from
@@ -47,9 +47,19 @@ function L.packages()
   return { 'lukas-reineke/indent-blankline.nvim' }
 end
 
+--- Whether a given blankline layer config is such that the rainbow layer should
+--- be enabled or not
+--- @param config table A config for the blankline layer
+--- @return boolean res True just in case the config is asking for the rainbow
+--- integration to be enabled, and the rainbow layer itself is also enable at
+--- the same time
+local function is_rainbow_integration_enabled(config)
+  return config.rainbow_integration and loader.layers.rainbow ~= nil
+end
+
 --- Adds integration with the rainbow layer according to:
 --- https://github.com/lukas-reineke/indent-blankline.nvim?tab=readme-ov-file
---- #rainbow-delimitersnvim-integration
+--- #rainbow-delimitersnvim-integration (up to ibl layer setup excluded)
 --- @param config table Current layer config
 local function add_rainbow_integration(config)
   local highlight = vim.tbl_map(function(v)
@@ -61,20 +71,34 @@ local function add_rainbow_integration(config)
       vim.api.nvim_set_hl(0, value.name, { fg = value.fg })
     end
   end)
-  vim.g.rainbow_delimiters.highlight = highlight
+  loader.get('rainbow').config.highlight = highlight
   config.indent_blankline.scope.highlight = highlight
+end
+
+--- Adds integration with the rainbow layer according to:
+--- https://github.com/lukas-reineke/indent-blankline.nvim?tab=readme-ov-file
+--- #rainbow-delimitersnvim-integration (from after ibl layer setup)
+local function register_rainbow_integration_hooks()
+  local hooks = get_module 'ibl.hooks'
   hooks.register(
     hooks.type.SCOPE_HIGHLIGHT,
     hooks.builtin.scope_highlight_from_extmark
   )
 end
 
-function L.load()
+function L.preload()
   local config = L.config
-  if config.rainbow_integration and layers.rainbow ~= nil then
+  if is_rainbow_integration_enabled(config) then
     add_rainbow_integration(config)
   end
+end
+
+function L.load()
+  local config = L.config
   get_module('ibl').setup(config.indent_blankline or {})
+  if is_rainbow_integration_enabled(config) then
+    register_rainbow_integration_hooks()
+  end
 end
 
 return L
