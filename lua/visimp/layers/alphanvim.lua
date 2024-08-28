cf32231a9c2f6de8184c5dbee87b3e616785d037
Mattia Girolimetto <mattia.girolimetto@studio.unibo.it>
2024-08-28 17:30:09 +0200

    chore: formatting

--- lua/visimp/layers/alphanvim.lua
+++ lua/visimp/layers/alphanvim.lua
@@ -1,50 +1,49 @@
 local L = require('visimp.layer').new_layer 'alphanvim'
 local get_module = require('visimp.bridge').get_module
-local fortune =  require('alpha.fortune')
+local fortune = require 'alpha.fortune'
 
 function L.packages()
   return { 'goolord/alpha-nvim' }
 end
 
-function default_layout()
-   local header = {
-    type = "text",
+local function default_layout()
+  local header = {
+    type = 'text',
     val = {
-  " _    __                     ",
-  "| |  / (_)____(_)___ ___  ____ ",
-  "| | / / / ___/ / __ `__ \\/ __ \\",
-  "| |/ / (__  ) / / / / / / /_/ /",
-  "|___/_/____/_/_/ /_/ /_/ .___/ ",
-  "                      /_/",
+      ' _    __                     ',
+      '| |  / (_)____(_)___ ___  ____ ',
+      '| | / / / ___/ / __ `__ \\/ __ \\',
+      '| |/ / (__  ) / / / / / / /_/ /',
+      '|___/_/____/_/_/ /_/ /_/ .___/ ',
+      '                      /_/',
     },
     opts = {
-        position = "center",
-        hl = "Type",
+      position = 'center',
+      hl = 'Type',
     },
-}
+  }
 
   local footer = {
-    type = "text",
+    type = 'text',
     val = fortune(),
     opts = {
-        position = "center",
-        hl = "Number",
+      position = 'center',
+      hl = 'Number',
     },
   }
 
   return {
     layout = {
-      { type = "padding", val = 12 },
+      { type = 'padding', val = 12 },
       header,
-      { type = "padding", val = 2 },
+      { type = 'padding', val = 2 },
       footer,
     },
     opts = {
-        margin = 5,
+      margin = 5,
     },
   }
 end
-
 
 L.default_config = default_layout()
 
