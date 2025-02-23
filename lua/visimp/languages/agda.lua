57623717755fed264bc16d0bc8ad14cac86e265b
Stefano Volpe <foxy@teapot.ovh>
2025-02-23 15:50:58 +0100

    feat: agda LS

--- lua/visimp/languages/agda.lua
+++ lua/visimp/languages/agda.lua
@@ -142,10 +142,8 @@ function L.grammars()
   return { 'agda' }
 end
 
--- function L.server()
---[[ TODO: https://github.com/mason-org/mason-registry/pull/5263 06-04-24,
-             Stefano Volpe foxy@teapot.ovh ]]
---   return 'agda-language-server'
--- end
+function L.server()
+  return 'agda_ls'
+end
 
 return L
