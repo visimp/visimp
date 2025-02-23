15d09b997760890c814e6369b5a3dccf873b0112
Stefano Volpe <foxy@teapot.ovh>
2025-02-23 16:55:45 +0100

    feat: rename coq to rocq

--- lua/visimp/languages/coq.lua
+++ lua/visimp/languages/coq.lua
@@ -1,13 +1,3 @@
-local layer = require 'visimp.layer'
-local L = layer.new_layer 'coq'
-
-function L.packages()
-  return { 'whonore/Coqtail' }
-end
-
-function L.load()
-  vim.cmd 'packadd Coqtail'
-  layer.to_vimscript_config(L, 'coqtail_', true)
-end
-
-return L
+local coqLayer = vim.deepcopy(require 'visimp.languages.rocq')
+coqLayer.identifier = 'coq'
+return coqLayer
