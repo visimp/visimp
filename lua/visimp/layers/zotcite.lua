f26510969ed11e50dad5a9dd5fcc4ecf4e868472
Stefano Volpe <foxy@teapot.ovh>
2025-08-14 18:03:04 +0200

    chore: empty zotcite config

--- lua/visimp/layers/zotcite.lua
+++ lua/visimp/layers/zotcite.lua
@@ -1,21 +1,8 @@
 local L = require('visimp.layer'):new_layer 'zotcite'
 local get_module = require('visimp.bridge').get_module
 
----All fields from https://github.com/windwp/nvim-autopairs#default-values
----are accepted here. Extra fields are `cmp_integration` and `html`, and default
----to true.
-L.default_config = {
-  bib_and_vt = {
-    markdown = true,
-    pandoc = false,
-    vimwiki = false,
-    rmd = false,
-    quarto = false,
-    typst = true,
-    latex = true,
-    rnoweb = true,
-  },
-}
+---All fields from https://github.com/jalvesaq/zotcite are accepted here.
+L.default_config = {}
 
 function L.dependencies()
   return { 'telescope', 'cmp' }
