749a3b13693fd5372d060300541165c39a5a92fe
Stefano Volpe <foxy@teapot.ovh>
2025-08-14 18:57:31 +0200

    feat: `papis` layer (#161)
    A layer offering integration with papis, the bibliography manager.

---
+++ lua/visimp/layers/papis.lua
@@ -0,0 +1,58 @@
+local L = require('visimp.layer'):new_layer 'papis'
+local get_module = require('visimp.bridge').get_module
+local layers = require 'visimp.loader'
+
+-- See https://github.com/jghauser/papis.nvim?tab=readme-ov-file#setup
+L.default_config = {}
+
+function L.dependencies()
+  return { 'telescope', 'cmp', 'treesitter' }
+end
+
+function L.packages()
+  return {
+    'jghauser/papis.nvim',
+    'kkharji/sqlite.lua',
+    'MunifTanjim/nui.nvim',
+    'pysan3/pathlib.nvim',
+    'nvim-neotest/nvim-nio',
+    'folke/snacks.nvim',
+  }
+end
+
+---Ensures the YAML Tree-sitter grammar is installed (if it is required).
+local function install_ts_yaml()
+  local config = L.config
+  if config and config.enable_modules and config.enable_modules.completion then
+    local ts_layer = layers.get 'treesitter' --[[@as TreesitterLayer]]
+    ts_layer:langs { 'yaml' }
+  end
+end
+
+---Looks for the initial `init_filetypes` field in the user config. If this has
+---not been specified, it is automatically populated based on the enabled
+---language layers.
+local function populate_init_filetypes()
+  if L.config and not L.config.init_filetypes then
+    local languages = layers.get('languages').config
+    local init_filetypes = {}
+    if languages.latex then
+      table.insert(init_filetypes, 'tex')
+    end
+    if languages.markdown then
+      table.insert(init_filetypes, 'markdown')
+      table.insert(init_filetypes, 'rmd')
+    end
+    if languages.typst then
+      table.insert(init_filetypes, 'typst')
+    end
+  end
+end
+
+function L.load()
+  install_ts_yaml()
+  populate_init_filetypes()
+  get_module('papis').setup(L.config)
+end
+
+return L
