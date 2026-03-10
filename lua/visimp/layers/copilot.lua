f36bd2e2936b36018aa5d5cfbea00d47381118f6
mattia <mgiro2001@gmail.com>
2026-03-10 17:10:29 +0100

    fix: conversion to viscript config

--- lua/visimp/layers/copilot.lua
+++ lua/visimp/layers/copilot.lua
@@ -1,9 +1,14 @@
 local L = require('visimp.layer'):new_layer 'copilot'
+local layer = require 'visimp.layer'
 
 function L.packages()
   return {
     'github/copilot.vim',
   }
+end
+
+function L.load()
+  layer.to_vimscript_config(L, 'copilot_', true)
 end
 
 return L
